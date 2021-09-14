d055ce1ea7d7918134401ff17a4a17e0caeab362
Felipe Magrassi <felipe.1magrassi@gmail.com>
2021-09-14 00:08:22 -0300

    part4: 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -15,8 +15,42 @@ const favoriteBlog = (blogs) => {
   );
 };
 
+const mostBlogs = (blogs) => {
+  const counter = [];
+
+  blogs.forEach((post) => {
+    const objIndex = counter.findIndex((obj) => obj.author === post.author);
+    if (objIndex > 0) {
+      counter[objIndex].blogs++;
+    } else {
+      counter.push({ author: post.author, blogs: 1 });
+    }
+  });
+  return counter.reduce((prev, current) =>
+    prev.blogs > current.blogs ? prev : current
+  );
+};
+
+const mostLikes = (blogs) => {
+  const counter = [];
+
+  blogs.forEach((post) => {
+    const objIndex = counter.findIndex((obj) => obj.author === post.author);
+    if (objIndex > 0) {
+      counter[objIndex].likes += post.likes;
+    } else {
+      counter.push({ author: post.author, likes: post.likes });
+    }
+  });
+  return counter.reduce((prev, current) =>
+    prev.likes > current.likes ? prev : current
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
