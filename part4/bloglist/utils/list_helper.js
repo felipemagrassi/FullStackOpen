0024f976be96ecbf07d04502171a952d7ba4f3ea
Felipe Magrassi <felipe.1magrassi@gmail.com>
2021-09-13 22:59:51 -0300

    total likes and favorite blog working

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (acumulador, valorAtual) => acumulador + valorAtual.likes,
+    0
+  );
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((prev, current) =>
+    prev.likes > current.likes ? prev : current
+  );
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
