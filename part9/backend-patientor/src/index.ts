a909f1c5416bd8ca89381adc936f5ca8008cc0c2
Felipe Magrassi <felipe.1magrassi@gmail.com>
2021-11-01 12:15:05 -0300

    9.15

--- part9/backend-patientor/src/index.ts
+++ part9/backend-patientor/src/index.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import cors from "cors";
-import diagnosesRouter from "./routes/diagnoses";
-import patientsRouter from "./routes/patients";
+import express from 'express';
+import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/diagnoses", diagnosesRouter);
-app.use("/api/patients", patientsRouter);
-app.get("/api/ping", (_req, res) => {
-  res.send("pong");
+app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
 });
 const PORT = 3001;
 app.listen(PORT, () => {
--- part9/backend-patientor/src/routes/patients.ts
+++ part9/backend-patientor/src/routes/patients.ts
@@ -1,18 +1,17 @@
-import express from "express";
-import patientsService from "../services/patientsService";
-import newPatientEntry from "../utils/parsePatient";
+import express from 'express';
+import patientsService from '../services/patientsService';
+import newPatientEntry from '../utils/parsePatient';
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
+router.get('/', (_req, res) => {
   const patients = patientsService.getPatientsWithoutSSN();
   return res.json(patients);
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   try {
     const newPatient = newPatientEntry(req.body);
-
     const patient = patientsService.addEntry(newPatient);
     res.json(patient);
   } catch (error: unknown) {
